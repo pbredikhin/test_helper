00f4ccbd7cd8ce7a4d87990d1862517766040050
pbredikhin <pbredikhin@activewizards.com>
2017-03-29 13:36:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Daniel Liu, Volodymyr Rospotniuk',
   author_email = 'hpec.liu@gmail.com, rospotniuk@gmail.com',
   url = 'https://github.com/rospotniuk/test_helper',
-  download_url = 'https://github.com/hpec/test_helper/tarball/0.1',
+  download_url = 'https://github.com/pbredikhin/test_helper/tarball/0.1',
   keywords = ['testing', 'autograder', 'image'],
   classifiers = [],
 )
